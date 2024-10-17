df30b6001a085b91da8f9fc71fcc9e5fd30e67a5
Bhavanamv19 <bhavanam.acharya.ac.in>
2024-10-17 12:14:17 +0530

    added

---
+++ markspy1.py
@@ -0,0 +1,25 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================================================== RESTART: C:/Users/bhava/AppData/Local/Programs/Python/Python311/largepython.py ====================================================
+Enter the first number: 4
+Enter the second number: 6
+Enter the third number: 8
+The largest number is: 8.0
+>>> 
+>>> 
+==================================================== RESTART: C:/Users/bhava/AppData/Local/Programs/Python/Python311/largepython.py ====================================================
+Enter Mark 1
+Traceback (most recent call last):
+  File "C:/Users/bhava/AppData/Local/Programs/Python/Python311/largepython.py", line 1, in <module>
+    a=int(input("Enter Mark 1"))
+ValueError: invalid literal for int() with base 10: ''
+>>> 45
+45
+>>> 
+==================================================== RESTART: C:/Users/bhava/AppData/Local/Programs/Python/Python311/largepython.py ====================================================
+Enter Mark 145
+Enter Mark 267
+Enter Mark 389
+B and C is big 78.0
+>>> 
